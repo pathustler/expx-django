8d15d81ddf8cfa68c2e82fe10431d7beaa0b70fc
DOCEA007 <docea007@gmail.com>
2023-06-16 18:10:17 +0300

    Repaired the serializers.py file after accidentaly deleting its contents (sorry)

--- expx/skills/serializers.py
+++ expx/skills/serializers.py
@@ -1,4 +1,19 @@
+from rest_framework import serializers
+from skills.models import SkillModel, ReviewModel, DayModel
 
+
+class SkillModel_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkillModel
+        fields = '__all__'
+        
+        
+class ReviewModel_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewModel
+        fields = '__all__'
+        
+        
 class DayModel_serializer(serializers.ModelSerializer):
     class Meta:
         model = DayModel
